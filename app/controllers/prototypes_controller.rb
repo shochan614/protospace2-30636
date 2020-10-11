05df224ba28697cd50fde3a4642905c9abaf5071
shochan614 <shochan.hina@gmail.com>
2020-10-11 21:53:44 +0900

    新規投稿機能実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,6 +7,12 @@ class PrototypesController < ApplicationController
   end
 
   def create 
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 private
