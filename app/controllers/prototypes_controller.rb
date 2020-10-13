2ff893c2841bd29fac853eeffcf2dbc5a5076de9
shochan614 <shochan.hina@gmail.com>
2020-10-13 11:15:19 +0900

    ログアウト状態でもプロトタイプの一覧を可能に

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, expect: :show
+  before_action :authenticate_user!, only: :new
   before_action :move_to_index, only: :edit
   def index
     @prototypes = Prototype.all
